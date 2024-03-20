5cafb1a3ad2f84cdb08c0720ddaeccdd0c1607cd
Seerj1o <163345198+Seerj1o@users.noreply.github.com>
2024-03-20 16:45:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("estamos coisando em javaScript");
 console.log("Segunda linha coisando em javaScript");
 console.log("Terceira linha coisando em javaScript");
+console.log("Quarta linha coisando em javaScript");
